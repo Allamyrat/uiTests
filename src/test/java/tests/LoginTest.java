d90f789a51cf1174d9872d76b125231284378ea7
zohreD <zdurdyyewa@gmail.com>
2022-01-07 10:32:20 +0700

    Owner added

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,20 @@
 package tests;
 
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+
 public class LoginTest {
+
+    @Test
+    public void Login(){
+        step("Login", () -> {
+            Selenide.open();
+        });
+    }
+
+
 }
