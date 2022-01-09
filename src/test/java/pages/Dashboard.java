adcbeb6ac7ecf67f0ad36526930e074f04f0a6de
zohreD <zdurdyyewa@gmail.com>
2022-01-09 10:10:55 +0700

    navigate method changes

--- src/test/java/pages/Dashboard.java
+++ src/test/java/pages/Dashboard.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
 import utils.Hooks;
 
 import static com.codeborne.selenide.Selectors.byText;
@@ -9,7 +10,15 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class Dashboard extends Hooks {
 
+
     public void navigateToTabs(String tab){
-        $("li").$(byText(tab)).click();
+      $(byText(tab)).click();
+    }
+
+    @Test
+    public void tttt() {
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+        $(".oe_menu_text").$(byText("Calendar")).click();
     }
 }
--- src/test/java/tests/DashboardTabsNavigation.java
+++ src/test/java/tests/DashboardTabsNavigation.java
@@ -1,4 +1,45 @@
 package tests;
 
-public class DashboardTabsNavigation {
+import org.junit.jupiter.api.Test;
+import pages.Dashboard;
+import pages.LoginPage;
+
+import static io.qameta.allure.Allure.step;
+
+public class DashboardTabsNavigation extends Dashboard {
+
+    LoginPage loginPage=new LoginPage();
+
+    @Test
+    public void tabsNavigationTest(){
+
+        step("login", () ->{
+            loginPage.login();
+        });
+        step("navigate to Discuss tab", () -> {
+            navigateToTabs("Discuss");
+        });
+        step("navigate to Calendar tab", () -> {
+            navigateToTabs("Calendar");
+        });
+        step("navigate to Notes tab", () -> {
+            navigateToTabs("Notes");
+        });
+        step("navigate to Contacts tab", () -> {
+            navigateToTabs("Contacts");
+        });
+        step("navigate to CRM tab", () -> {
+            navigateToTabs("CRM");
+        });
+        step("navigate to Sales tab", () -> {
+            navigateToTabs("Sales");
+        });
+        step("navigate to Point of Sale tab", () -> {
+            navigateToTabs("Point of Sale");
+        });
+        step("navigate to Purchases tab", () -> {
+            navigateToTabs("Purchases");
+        });
+
+    }
 }
